d85d88c42bf424192e1084b175ced145976da2db
justin <fezzygo@gmail.com>
2022-06-05 09:47:22 -0400

    add search function (js:1-7,12-31), updated dropdown options (HTML:21-28)

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,31 @@
+// Selectors 
+var searchInput = document.querySelector("#search-input");
+var searchBtn = document.querySelector("#search-btn");
+var searchType = document.querySelector("#salutation");
 
+// Event listener
+searchBtn.addEventListener("click", runSearch);
 
 // Select Menu
 $( "#salutation" ).selectmenu();
 
+locBaseUrl = "https://www.loc.gov/";
+jsonParam = "&fo=json";
+var searchURL= '';
+
+function runSearch(){
+    searchURL=`${locBaseUrl}?q=${searchInput.value}${jsonParam}`;
+
+    if (searchType.value !== 'Select a format...'){
+        searchURL=`${locBaseUrl}${searchType.value}/?q=${searchInput.value}${jsonParam}`;
+    }
+
+    fetch(searchURL)
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+
+}
